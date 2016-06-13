9bc8f0bb630a0da5661ad01162637f3621db1b8d
半边 <itaofe@gmail.com>
2016-06-13 23:26:35 +0800

    add qiniu function 1st

--- module/pub.js
+++ module/pub.js
@@ -2,7 +2,7 @@
 let program = require('commander')
 program.command('pub [tag_name]', '发布dist目录下文件到CDN(若指定tag_name，将打一个git tag并推送)')
   .action(function() {
-    console.log(this);
+    console.log('pub start');
   })
 
 
