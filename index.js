1cf6c1fcc3694ec9a4c874830b4c6016d8860965
半边 <itaofe@gmail.com>
2016-06-13 18:07:11 +0800

    init wil,add pub first

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+let program = require('commander')
+
+program
+  .version('0.0.2')
+  //.option('pub', 'enable some bar')
+  .option('-B, --baz', 'enable some baz')
+
+
+// require ./module/xx.js
+let normalizedPath = require("path").join(__dirname, "module")
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+  if (/\.js$/.test(file)) {
+    require("./module/" + file);
+  }
+})
+
+program.parse(process.argv)
+if (!program.args.length) program.help()
+
---
+++ module/pub.js
@@ -0,0 +1,8 @@
+'use strict';
+let program = require('commander')
+program.command('pub [tag_name]', '发布dist目录下文件到CDN(若指定tag_name，将打一个git tag并推送)')
+  .action(function() {
+    console.log(this);
+  })
+
+
