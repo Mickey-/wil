bec7befc26cd63e903419ed7137772d16117ab67
半边 <itaofe@gmail.com>
2016-06-14 17:19:43 +0800

    es6 style update

--- module/pub.js
+++ module/pub.js
@@ -1,5 +1,5 @@
 'use strict';
-let program = require('commander')
+const program = require('commander')
 program.command('pub [tag_name]', '发布dist目录下文件到CDN(若指定tag_name，将打一个git tag并推送)')
   .action(function() {
     console.log('pub start');
