6b9ffef1b05b4366fa910e89e833c1c53eb957b8
半边 <itaofe@gmail.com>
2016-06-23 17:55:52 +0800

    resolve [no git remote get-url in low version git]

--- plugin/pub.js
+++ plugin/pub.js
@@ -3,7 +3,7 @@ const program = require('commander')
 const chalk = require("chalk")
 program.command('pub [tag_name]', '发布dist目录下文件到CDN(若指定tag_name，将打一个git tag并推送)')
   .action(function() {
-    console.log(chalk.cyan('[INFO] ') + '读取项目根目录配置文件.wilrc，自动化发布开始...\n');
+    console.log(chalk.cyan('[INFO] ') + '解析项目根目录的可选配置文件.wilrc, 启动自动化部署...');
   })
 
 
