d9626caad87ce51c9d916358aaff8c3aaddc9844
半边 <itaofe@gmail.com>
2016-06-23 14:58:19 +0800

    add push tag for wil pub

---
+++ module/pushTag.js
@@ -0,0 +1,16 @@
+'use strict';
+const exec = require('child-process-promise').exec
+const chalk = require("chalk")
+
+module.exports = (tagName) => {
+  exec(`git tag ${tagName} && git push origin ${tagName}`)
+    .then((ret) => {
+      const stdout = ret.stdout
+      console.log(stdout)
+      console.log(chalk.green('[SUCCESS] ') + chalk.cyan(`tag 【${tagName}】 推送成功！`))
+    })
+    .catch((err) => {
+      console.log(chalk.red('[ERROR] 打tag操作失败！ ') + '静态文件发布继续...')
+      console.error(chalk.red('[ERROR] '), err);
+    })
+}
