e0c2dff7ee083e0fbb986c085763bbdc30fd5725
半边 <itaofe@gmail.com>
2016-06-23 14:58:51 +0800

    improve commander exp

--- plugin/pub.js
+++ plugin/pub.js
@@ -1,8 +1,9 @@
 'use strict';
 const program = require('commander')
+const chalk = require("chalk")
 program.command('pub [tag_name]', '发布dist目录下文件到CDN(若指定tag_name，将打一个git tag并推送)')
   .action(function() {
-    console.log('pub start');
+    console.log(chalk.cyan('[INFO] ') + '读取项目根目录配置文件.wilrc，自动化发布开始...\n');
   })
 
 
