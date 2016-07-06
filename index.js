c07b160fe9839dc3f2ba3e3701576d6c3445c35d
半边 <itaofe@gmail.com>
2016-07-06 12:27:30 +0800

    remove plugins dir,merge into index.js

--- index.js
+++ index.js
@@ -4,17 +4,11 @@ const program = require('commander')
 program
   .version('0.0.9')
   .description('59前端工作流集成解决方案')
-  //.option('pub', 'enable some bar')
+  .usage('<command> [options]')
   .option('-B, --baz', 'enable some baz')
 
 
-// require ./module/xx.js
-let normalizedPath = require("path").join(__dirname, "plugin")
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
-  if (/\.js$/.test(file)) {
-    require("./plugin/" + file)
-  }
-})
+program.command('pub [path] [options]', '部署发布目录(默认dist/)下文件到CDN。若指定path(默认./)，将只发布指定文件(夹)')
 
 program.parse(process.argv)
 if (!program.args.length) program.help()
