4c183a5ba6ef94f5568ab4966bfab01bced23052
半边 <itaofe@gmail.com>
2016-07-07 15:38:09 +0800

    up ver; 0.0.10

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const program = require('commander')
 
 program
-  .version('0.0.9')
+  .version('0.0.10')
   .description('59前端工作流集成解决方案')
   .usage('<command> [options]')
   .option('-B, --baz', 'enable some baz')
