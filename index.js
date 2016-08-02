e3383aca68aecce41ec3d4a9cfc6902242ccd615
半边 <itaofe@gmail.com>
2016-08-02 20:13:21 +0800

    add wil init

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ program
 
 
 program.command('pub [path] [options]', '部署发布目录(默认dist/)下文件到CDN。若指定path(默认./)，将只发布指定文件(夹)')
+program.command('init [options]', '初始化一个react全家桶项目')
 
 program.parse(process.argv)
 if (!program.args.length) program.help()
