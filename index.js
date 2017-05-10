e82b259310fd1d2f8f4ecd9b4600b53aa3230103
半边 <itaofe@gmail.com>
2017-05-10 15:34:28 +0800

    支持强制发布非dist目录下的文件。禁止发布非当前项目仓库外的文件

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ program
   .version('0.2.4')
   .description('骑迹前端工作流集成解决方案')
   .usage('<command> [options]')
-  .option('-B, --baz', 'enable some baz')
 
 
 program.command('pub [path] [options]', '部署发布目录(默认dist/)下文件到CDN。若指定path(默认./)，将只发布指定文件(夹)')
